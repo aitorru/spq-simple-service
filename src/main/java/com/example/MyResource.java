5e51805e0874e3d82d4e7b8b3473ea7dd84d521a
Aitor Ruiz <aitorruiz2m@gmail.com>
2021-03-26 09:02:59 +0100

    Changed the language.

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -12,14 +12,14 @@ import jakarta.ws.rs.core.MediaType;
 public class MyResource {
 
     /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
+     * Method handling HTTP GET requests. The returned object will be sent to the
+     * client as "text/plain" media type.
      *
      * @return String that will be returned as a text/plain response.
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        return "Lo tengo!";
     }
 }
